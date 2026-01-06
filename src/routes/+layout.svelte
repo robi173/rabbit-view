df596e21ed0fd676e6c7475d8f8fa019fafa1a5f
robi173 <ninic.robert@johak.at>
2026-01-07 00:17:22 +0100

    BisV5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script>
 	import '../app.css';
+	import { store } from '$lib/store.svelte.js';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
-	let theme = $state('cyberpunk');
+	let theme = $state('halloween');
+
+	// unser "Konstruktor" (lifecycle hook) - läuft jedesmal, wenn die Seite bzw. die Komponente geladen wird:
+	$effect(() => {
+		store.listRabbits();
+		$inspect('🐰: ', store.rabbits);
+	});
 </script>
 
 <svelte:head>
@@ -11,7 +18,9 @@
 </svelte:head>
 
 <div data-theme={theme} class=" min-h-screen p-2">
-	<select class="select absolute top-2 right-2 w-[150px]" bind:value={theme}>
+	<nav class="flex items-center justify-between">
+		<a href="/" class="text-4xl">🐰</a>
+	<select class="select w-[150px]" bind:value={theme}>
 		<option disabled selected>Pick a theme</option>
 		<option value="light">light</option>
 		<option value="dark">dark</option>
@@ -19,5 +28,8 @@
 		<option value="valentine">valentine</option>
 		<option value="halloween">halloween</option>
 	</select>
+</nav>
+<main class="flex flex-col items-center justify-center">
 	{@render children?.()}
+</main>
 </div>
