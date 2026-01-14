029cb0d14291ad35ac40cc2c50f12dabf49b50ea
robi173 <ninic.robert@johak.at>
2026-01-14 13:08:21 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,6 +13,7 @@
 	// unser "Konstruktor" (lifecycle hook) - läuft jedesmal, wenn die Seite bzw. die Komponente geladen wird:
 	$effect(() => {
 		store.listRabbits();
+		if(localStorage.getItem("theme"))
 		theme = localStorage.getItem("theme");
 		$inspect('🐰: ', store.rabbits);
 	});
@@ -25,17 +26,16 @@
 <div data-theme={theme} class=" min-h-screen p-2">
 	<nav class="flex items-center justify-between">
 		<a href="/" class="text-4xl">🐰</a>
-	<div>
-		<button class="btn" onclick={saveTheme}>Save Theme!</button>
+	
 		<select class="select w-[150px]" bind:value={theme}>
+
 		<option disabled selected>Pick a theme</option>
-		<option value="light">light</option>
-		<option value="dark">dark</option>
-		<option value="cyberpunk">cyberpunk</option>
-		<option value="valentine">valentine</option>
-		<option value="halloween">halloween</option>
+		<option value="light" onclick={saveTheme}>light</option>
+		<option value="dark" onclick={saveTheme}>dark</option>
+		<option value="cyberpunk" onclick={saveTheme}>cyberpunk</option>
+		<option value="valentine" onclick={saveTheme}>valentine</option>
+		<option value="halloween" onclick={saveTheme}>halloween</option>
 	</select>
-	</div>
 </nav>
 <main class="flex flex-col items-center justify-center">
 	{@render children?.()}
